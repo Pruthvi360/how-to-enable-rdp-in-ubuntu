0b20a6a5e3beb2ea8df791c68eed317ff6020e0c
Pruthvi_cloud_deveops <107435692+Pruthvi360@users.noreply.github.com>
2023-01-24 19:39:01 +0530

    Create ubuntu-desktop.sh

---
+++ ubuntu-desktop.sh
@@ -0,0 +1,9 @@
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install lightdm
+sudo systemctl start lightdm
+sudo apt-get install tasksel
+sudo tasksel install ubuntu-desktop
+#sudo apt-get install ubuntu-desktop
+sudo tasksel install xubuntu-desktop
+sudo tasksel install lubuntu coresudo tasksel install ubuntu mate
+reboot
