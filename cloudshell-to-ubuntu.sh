3e76a76289852b81cc0d8048708779cb38be71af
Pruthvi_cloud_deveops <107435692+Pruthvi360@users.noreply.github.com>
2023-03-25 13:44:11 +0530

    Update cloudshell-to-ubuntu.sh

--- cloudshell-to-ubuntu.sh
+++ cloudshell-to-ubuntu.sh
@@ -1,6 +1,6 @@
 sudo su
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-sudo apt install ./chrome-remote-desktop_current_amd64.deb
+sudo apt install ./chrome-remote-desktop_current_amd64.deb -y
 
 ##XFCE Desktop Environment:
  
